cb9e72a199c999a96edcf94d1ae524371a76663f
Anthony Gagliano <antogagliano3@gmail.com>
2023-01-28 13:32:46 -0500

    Server setup

---
+++ questions/urls.py
@@ -0,0 +1,12 @@
+# cards/urls.py
+
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path(
+        "",
+        TemplateView.as_view(template_name="cards/base.html"),
+        name="home"
+    ),
+]
